f94c3be94a304eccd2517f63b729afe41bc08272
Mathieu Noverraz <mathieu.noverraz@gmail.com>
2012-09-27 16:43:32 +0200

    wip

--- controllers/utils.php
+++ controllers/utils.php
@@ -13,4 +13,10 @@ class UtilsController extends xWebController {
 		xContext::$front->setup_i18n($langString);		
 	}
 	
+	function pdfurl($url) {
+		$is_pdf = !isset($_REQUEST['html']);
+		if (!$is_pdf) return $url;
+		$file = preg_replace('/.*a(.+)/', 'file://'.xContext::$basepath.'/public/a$1', $url);
+		return $file;
+	}
 }
